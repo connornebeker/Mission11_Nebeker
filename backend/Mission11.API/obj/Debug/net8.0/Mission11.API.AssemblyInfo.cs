a070d36c1769b4ca46281229faf419559145d02e
connornebeker <connor.nebeker03@gmail.com>
2025-04-03 16:02:15 -0600

    deployment prep

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c49f88ab36fdea3441492840d2aae34cf89685f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1eb31c8f0838befecc5ed14ac3781c1201a1a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Mission11.API/obj/release/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/release/net8.0/Mission11.API.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28551751bbd21daf24c27844db49bfa9636242fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1eb31c8f0838befecc5ed14ac3781c1201a1a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
