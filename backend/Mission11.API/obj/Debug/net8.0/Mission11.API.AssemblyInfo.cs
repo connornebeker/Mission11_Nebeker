abbf5ae92f81291bc87e88ff20fe609f0915e8cc
connornebeker <connor.nebeker03@gmail.com>
2025-04-03 15:31:37 -0600

    deployment

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db1f9cd1b3552d36cb7220a27adaaeaa40ae8c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c49f88ab36fdea3441492840d2aae34cf89685f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
